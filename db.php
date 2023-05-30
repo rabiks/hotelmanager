8526b550a43cb4722cfff8778bff63bc85f02b42
rabiks <sr@2rsystems.pl>
2023-05-30 18:46:47 +0200

    dodano plik db.php łączący naszą aplikacje z bazą danych mysql - na xampp

---
+++ db.php
@@ -0,0 +1,56 @@
+<?php
+// Połączenie z bazą danych
+// w tym przypadku łączymy się z lokalną bazą XAMPP
+$servername = "localhost";
+$username = "rabiks-local";
+$password = "BardzoTajne!";
+$dbname = "rooms";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Klasa reprezentująca pokój
+class Room
+{
+    private $id;
+    private $image;
+
+    public function __construct($id, $image)
+    {
+        $this->id = $id;
+        $this->image = $image;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
+
+// Pobierz dane o pokojach z bazy danych i wyświetl je na frontendzie
+function getRooms()
+{
+    global $conn;
+    $sql = "SELECT * FROM rooms";
+    $result = $conn->query($sql);
+
+    $rooms = array();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $room = new Room($row['id'], $row['image']);
+            array_push($rooms, $room);
+        }
+    }
+
+    return $rooms;
+}
+?>
